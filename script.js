0a17798a031fdd9001a23b2c8f9fe2fe5ac636f2
Meow <50887230+itzTheMeow@users.noreply.github.com>
2020-12-24 02:57:50 -0500

    add links

---
+++ script.js
@@ -0,0 +1,30 @@
+function openLink(lnk) {
+  switch (lnk) {
+    case "discord":
+      window.open("https://discord.gg/fEY6bzKt3U", "_blank");
+      break;
+    case "github":
+      window.open("https://github.com/itzTheMeow", "_blank");
+      break;
+    case "youtube":
+      window.open("https://www.youtube.com/channel/UCWANi1TTqUP1ar4VTlOrqDA", "_blank");
+      break;
+    case "reddit":
+      window.open("https://www.reddit.com/user/itzTheMeow_", "_blank");
+      break;
+    case "twitter":
+      window.open("https://twitter.com/ALEXMEOW4560", "_blank");
+      break;
+    case "twitch":
+      window.open("https://www.twitch.tv/alexmeow4560", "_blank");
+      break;
+    case "instagram":
+      window.open("https://www.instagram.com/meowcatpersonthing/", "_blank");
+      break;
+    case "steam":
+      window.open("https://steamcommunity.com/id/ALEXMEOW4560", "_blank");
+      break;
+    default:
+      alert("NO");
+  }
+}
