accb99261fa08dcf7c6da4cc513a41b043f9d3ca
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-01-04 00:06:03 -0500

    add promise

--- people/script.js
+++ people/script.js
@@ -1,8 +1,17 @@
 let getToken = () => {
-  fetch("https://meowdotml-people-token.glitch.me/")
-    .then((res) => res.json())
-    .then((token) => {
-      console.log(token);
-    });
+  return new Promise((acc, rej) => {
+    fetch("https://meowdotml-people-token.glitch.me/")
+      .then((res) => res.json())
+      .then((token) => {
+        acc(token.access_token);
+      })
+      .catch((err) => {
+        alert(
+          `Error when authorizing with Discord API. Please try again or contact Meow with this error:
+      - ${err}`
+        );
+        rej(err);
+      });
+  });
 };
-getToken();
+getToken().then(console.log);
