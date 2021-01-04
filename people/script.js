545aaefadb55f2d7aebae5c49418ae180d636f98
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-01-04 03:03:28 -0500

    add flags

--- people/script.js
+++ people/script.js
@@ -1,4 +1,27 @@
 window.APIURL = "https://themeowdotmlbackend.herokuapp.com";
+window.FLAGS = {
+  DISCORD_EMPLOYEE: 1 << 0,
+  PARTNERED_SERVER_OWNER: 1 << 1,
+  DISCORD_PARTNER: 1 << 1,
+  HYPESQUAD_EVENTS: 1 << 2,
+  BUGHUNTER_LEVEL_1: 1 << 3,
+  HOUSE_BRAVERY: 1 << 6,
+  HOUSE_BRILLIANCE: 1 << 7,
+  HOUSE_BALANCE: 1 << 8,
+  EARLY_SUPPORTER: 1 << 9,
+  TEAM_USER: 1 << 10,
+  SYSTEM: 1 << 12,
+  BUGHUNTER_LEVEL_2: 1 << 14,
+  VERIFIED_BOT: 1 << 16,
+  EARLY_VERIFIED_DEVELOPER: 1 << 17,
+  VERIFIED_DEVELOPER: 1 << 17,
+};
+
+let serialize = function (...hasParams) {
+  const serialized = {};
+  for (const [flag, bit] of Object.entries(FLAGS)) serialized[flag] = (hasParams[0] & bit) === bit;
+  return serialized;
+};
 
 let getUser = (id, tkn) => {
   return new Promise((acc, rej) => {
@@ -7,14 +30,34 @@ let getUser = (id, tkn) => {
       .then((user) => {
         acc({
           ...user,
-          ...{ avatarURL: `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png` },
+          ...{
+            avatarURL: `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`,
+            tag: `${user.username}#${user.discriminator}`,
+            flags: serialize(user.public_flags),
+          },
         });
       })
       .catch(rej);
   });
 };
+
 window.onload = function () {
-  getUser("609286417981505557").then((user) => {
-    console.log(user);
-  });
+  fetch(`${APIURL}/user/all`)
+    .then((res) => res.json())
+    .then((users) => {
+      users.forEach((u) => {
+        getUser(u)
+          .then((user) => {
+            console.log(user);
+          })
+          .catch((err) => {
+            alert(`Error fetching user. Please try again.
+Error: ${err}`);
+          });
+      });
+    })
+    .catch((err) => {
+      alert(`Error connecting to user fetch API. Please try again.
+Error: ${err}`);
+    });
 };
