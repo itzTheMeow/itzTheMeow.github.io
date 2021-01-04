729f2d330f3a9e9406fa844a164b6f386ba0a874
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-01-03 22:43:15 -0500

    fix?

--- people/script.js
+++ people/script.js
@@ -1,21 +1,26 @@
 let getToken = () => {
-  var url = "https://discord.com/api/oauth2/token";
+  var url = "https://discord.com/api/v8/oauth2/token";
 
-var xhr = new XMLHttpRequest();
-xhr.open("POST", url);
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", url);
 
-xhr.setRequestHeader("Authorization", "Basic Nzk1MTI2NTc0NjU0ODE2MjY3Om9pSGZJalRSWHZNWGNhMnRtZnVHdEZ4UUd2LVFudm5h");
-xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  //xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+  xhr.setRequestHeader(
+    "Authorization",
+    "Basic Nzk1MTI2NTc0NjU0ODE2MjY3Om9pSGZJalRSWHZNWGNhMnRtZnVHdEZ4UUd2LVFudm5h"
+  );
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
-xhr.onreadystatechange = function () {
-   if (xhr.readyState === 4) {
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
       console.log(xhr.status);
       console.log(xhr.responseText);
       console.log(JSON.stringify(xhr));
-   }};
+    }
+  };
 
-var data = "grant_type=client_credentials&scope=identify";
+  var data = "grant_type=client_credentials&scope=identify";
 
-xhr.send(data);
-}
-getToken()
+  xhr.send(data);
+};
+getToken();
