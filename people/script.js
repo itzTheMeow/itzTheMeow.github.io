66c914c98a3b917a6f776bb03607c0b55076ba39
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-01-03 22:06:28 -0500

    Update script.js

--- people/script.js
+++ people/script.js
@@ -11,6 +11,7 @@ xhr.onreadystatechange = function () {
    if (xhr.readyState === 4) {
       console.log(xhr.status);
       console.log(xhr.responseText);
+      console.log(xhr);
    }};
 
 var data = "grant_type=client_credentials&scope=identify";
