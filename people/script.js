62b17b6f005a373d9bee15dc5147e2ef833b647a
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-01-04 08:00:10 -0500

    move to server

--- people/script.js
+++ people/script.js
@@ -1,42 +1,11 @@
 window.APIURL = "https://themeowdotmlbackend.herokuapp.com";
-window.FLAGS = {
-  DISCORD_EMPLOYEE: 1 << 0,
-  PARTNERED_SERVER_OWNER: 1 << 1,
-  DISCORD_PARTNER: 1 << 1, // merge
-  HYPESQUAD_EVENTS: 1 << 2,
-  BUGHUNTER_LEVEL_1: 1 << 3,
-  HOUSE_BRAVERY: 1 << 6,
-  HOUSE_BRILLIANCE: 1 << 7,
-  HOUSE_BALANCE: 1 << 8,
-  EARLY_SUPPORTER: 1 << 9,
-  TEAM_USER: 1 << 10,
-  SYSTEM: 1 << 12,
-  BUGHUNTER_LEVEL_2: 1 << 14,
-  VERIFIED_BOT: 1 << 16,
-  EARLY_VERIFIED_DEVELOPER: 1 << 17,
-  VERIFIED_DEVELOPER: 1 << 17, // merge
-};
-
-let serialize = function (...hasParams) {
-  const serialized = {};
-  for (const [flag, bit] of Object.entries(FLAGS)) serialized[flag] = (hasParams[0] & bit) === bit;
-  return serialized;
-};
 
 let getUser = (id, tkn) => {
   return new Promise((acc, rej) => {
     fetch(`${APIURL}/user/${id}`)
       .then((res) => res.json())
       .then((user) => {
-        acc({
-          // move to server side, check for 'id' attribute
-          ...user,
-          ...{
-            avatarURL: `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`,
-            tag: `${user.username}#${user.discriminator}`,
-            flags: serialize(user.public_flags), // move to something like "bugHunter = 0/1/2" and "squad = 'bravery/brilliance/balance"
-          },
-        });
+        acc(user);
       })
       .catch(rej);
   });
