3e28ee72f856b9e2290d17101d396611d9f5f815
Meow <me@themeow.dev>
2021-01-03 21:40:49 -0500

    test

---
+++ people/script.js
@@ -0,0 +1,19 @@
+let getUser = () => {
+  var url = "https://discord.com/api/oauth2/token";
+
+var xhr = new XMLHttpRequest();
+xhr.open("POST", url);
+
+xhr.setRequestHeader("Authorization", "Basic Nzk1MTI2NTc0NjU0ODE2MjY3Om9pSGZJalRSWHZNWGNhMnRtZnVHdEZ4UUd2LVFudm5h");
+xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+xhr.onreadystatechange = function () {
+   if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+   }};
+
+var data = "grant_type=client_credentials&scope=identify";
+
+xhr.send(data);
+}
