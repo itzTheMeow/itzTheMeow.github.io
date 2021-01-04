980561bafee8f4ff68ae530afbe4231b0b705a45
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-01-04 00:41:28 -0500

    redo entire thing...

--- people/script.js
+++ people/script.js
@@ -1,29 +1,18 @@
-let getToken = () => {
-  return new Promise((acc, rej) => {
-    fetch("https://meowdotml-people-token.glitch.me/")
-      .then((res) => res.json())
-      .then((token) => {
-        acc(token.access_token);
-      })
-      .catch((err) => {
-        alert(
-          `Error when authorizing with Discord API. Please try again or contact Meow with this error:
-      - ${err}`
-        );
-        rej(err);
-      });
-  });
-};
 let getUser = (id, tkn) => {
   return new Promise((acc, rej) => {
-    fetch(`https://discord.com/api/v8/users/${id}`, {
-      headers: { Authorization: `Bearer ${tkn}` },
-    })
+    fetch(`https://meowdotml-people-token.glitch.me/user/${id}`)
       .then((res) => res.json())
-      .then(console.log)
+      .then((user) => {
+        acc({
+          ...user,
+          ...{ avatarURL: `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png` },
+        });
+      })
       .catch(rej);
   });
 };
-getToken().then((token) => {
-  getUser("609286417981505557", token);
-});
+window.onload = function () {
+  getUser("609286417981505557").then((user) => {
+    console.log(user);
+  });
+};
