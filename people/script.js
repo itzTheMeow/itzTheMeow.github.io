309d2902ba4cb017f39a173b896d8c7b14125586
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-01-04 00:00:33 -0500

    fix i guess...

--- people/script.js
+++ people/script.js
@@ -1,24 +1,8 @@
 let getToken = () => {
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://discord.com/api/v8/oauth2/token");
-
-  xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
-  xhr.setRequestHeader(
-    "Authorization",
-    "Basic Nzk1MTI2NTc0NjU0ODE2MjY3Om9pSGZJalRSWHZNWGNhMnRtZnVHdEZ4UUd2LVFudm5h"
-  );
-  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-      console.log(xhr.status);
-      console.log(xhr.responseText);
-      console.log(JSON.stringify(xhr));
-    }
-  };
-
-  var data = "grant_type=client_credentials&scope=identify";
-
-  xhr.send(data);
+  fetch("https://meowdotml-people-token.glitch.me/")
+    .then((res) => res.json())
+    .then((token) => {
+      console.log(token);
+    });
 };
 getToken();
